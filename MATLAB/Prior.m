85fb9a8e072cd29391b0247fb707d253d7dc7dda
Phil Ward <phillip.ward@monash.edu>
2017-03-22 19:23:24 +1100

    Added description to Prior.m

--- MATLAB/Prior.m
+++ MATLAB/Prior.m
@@ -1,6 +1,12 @@
 function Prior( maskFile, swiFile, qsmFile, freFile, veinFile, llSFile, llQFile, llFFile )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+%Prior - Calculates the log loss of three inputs
+%   For each input (swi, qsm and atlas) using a mask and a ground truth (veinFile)
+%   the log loss is calculatedafter applying a Gaussian Mixture Model to the SWI and QSM
+%   images. The atlas is used in raw format.
+%
+%   The binary vein mask is weighted as 0.1 and 0.9 for non-vein and vein voxels
+%   to incorporate uncertainty and to avoid log(0) errors.
+
 addpath(genpath('shared-src/'))
 
 mask = load_untouch_nii(maskFile);
